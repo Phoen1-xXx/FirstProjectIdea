3d9192c03ef6d966efe91d15bf9ce09a97f1053c
L <soselia.aleksandre@gmail.com>
2024-11-05 21:53:57 +0400

    First Commit of Code

---
+++ src/main/java/org/example/Test.java
@@ -0,0 +1,32 @@
+package org.example;
+
+
+public class Test {
+//    psvm or main
+    public static void main(String[] args) {
+
+        byte customerAge = 127;
+        short salary = 32767;
+        int salaryBig = 2147483647;
+        long longSalary = 9223372036854775807L;
+
+        float height = 1.65f;
+        double heightD = 3.43;
+
+        boolean t = true;
+
+        char k = 'k';
+
+        int a = 7;
+        int b = 3;
+
+        char aa= 'a';
+        char bb= 'b';
+
+        double c = (double) 7 /3;
+
+//        sout
+        System.out.println(c);
+
+    }
+}
